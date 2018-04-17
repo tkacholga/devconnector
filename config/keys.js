5912449bda3ea96429cb99ae5b7366354283e6fc
Olya <tkacholya@gmail.com>
2018-04-17 16:16:05 -0400

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://olga:olga@ds249249.mlab.com:49249/dev_connector"
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   Get api/posts/test
+// @desc    Tests post route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+module.exports = router;
